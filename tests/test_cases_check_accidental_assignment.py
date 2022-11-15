0ec067c3215ae71c28453b4fb150509355054567
Rafael Silva <perigoso@riseup.net>
2022-11-15 18:36:32 +0000

    test: add unit test for check_accidental_assignment
    test: fix wrong expected test result

---
+++ tests/test_cases_check_accidental_assignment.py
@@ -0,0 +1,338 @@
+
+# (test case name, expected result True=pass False=fail, extra arguments, input)
+
+test_cases = [
+        ('control', True, {'strict': True}, '''
+	int a = 0;
+'''),
+
+        ('strict', True, {'strict': False}, '''
+	(a = 0) ? 1 : 2;
+'''),
+
+        ('strict', False, {'strict': True}, '''
+	(a = 0) ? 1 : 2;
+'''),
+
+    ('skip-keyword', True, {}, '''
+	dummy_keyword(a = 1);
+'''),
+
+    ('skip-keyword', False, {'strict': True}, '''
+	dummy_keyword(a = 1);
+'''),
+
+    ('skip-keyword', True, {'strict': True, 'ignore_keywords': ['dummy_keyword']}, '''
+	dummy_keyword(a = 1);
+'''),
+
+    ('cpp-comment', True, {'strict': True},
+            '''
+int main(void)
+{
+	/*
+	 * if (inside comment = 1)
+	 *
+	 */
+}
+'''),
+
+    ('cpp-comment', True, {'strict': True},
+            '''
+	if (1 /* (inside comment = 1) */) {
+	{
+		/* code */
+	}
+'''),
+
+        ('cpp-comment', True, {'strict': True},
+         '''
+	if (1 /* (inside comment = 1)
+	* )
+	*/
+	) {
+	{
+		/* code */
+	}
+'''),
+
+    ('cpp-comment', True, {},
+            '''
+	while ((((dummy() /*/ a = 1 /* comment inside = 1 */))))
+	{
+		/* code */
+	}
+'''),
+
+    ('c-comment', True, {},
+        '''
+	for (int i = 0; // comment = 1
+	i < 10;
+	i++)
+	{
+		/* code */
+	}
+'''),
+
+    ('c-comment', True, {},
+        '''
+	for (int i = 0; // ;a=1;) for(;comment = 1;)
+	i < 10;
+	i++)
+	{
+		/* code */
+	}
+'''),
+
+    ('c-comment', True, {},
+        '''
+//	if (1)
+//	{
+//		/* code */
+//	}
+'''),
+
+    ('c-comment', True, {},
+        '''
+	if (1 //
+	)
+	{
+		/* code */
+	}
+'''),
+
+    ('if', True, {},
+            '''
+	if (1)
+	{
+		/* code */
+	}
+'''),
+
+
+    ('if', False, {},
+            '''
+	if (a = 1)
+	{
+		/* code */
+	}
+'''),
+
+    ('if', False, {},
+            '''
+	if(1 = dummy())
+	{
+		/* code */
+	}
+'''),
+
+    ('if', False, {},
+            '''
+	if ((((dummy() = 1))))
+	{
+		/* code */
+	}
+'''),
+
+    ('if', True, {},
+            '''
+	if((((1))))
+	{
+		/* code */
+	}
+'''),
+
+    ('for', True, {},
+            '''
+	for (int i = 0; i < 10; i++)
+	{
+		/* code */
+	}
+'''),
+
+    ('for', True, {},
+            '''
+	for(int i=0;i<10;i++)
+	{
+		/* code */
+	}
+'''),
+
+    ('for', False, {},
+            '''
+	for (size_t i = 0; (i = count); i = i + 1)
+	{
+		/* code */
+	}
+'''),
+
+    ('for', False, {},
+            '''
+	for (size_t i = 0; i = count; i = i + 1)
+	{
+		/* code */
+	}
+'''),
+
+    ('while', True, {},
+            '''
+	while (1)
+	{
+		/* code */
+	}
+'''),
+
+    ('while', False, {},
+            '''
+	while(1 = dummy())
+	{
+		/* code */
+	}
+'''),
+
+    ('while', False, {},
+            '''
+	while ((((dummy() = 1))))
+	{
+		/* code */
+	}
+'''),
+
+    ('return', True, {'strict': False},
+            '''
+	return (i = 10);
+'''),
+
+    ('return', False, {'strict': True},
+            '''
+	return (i = 10);
+'''),
+
+    ('return', False, {'strict': True},
+            '''
+	return ((i = 10
+	));
+'''),
+
+    ('return', True, {'strict': True},
+            '''
+	return ((i == 10
+	));
+'''),
+
+
+    ('string', False, {'strict': True},
+            '''
+	print(a = "Hello World = 10");
+'''),
+
+    ('string', True, {},
+            '''
+	print(a = "Hello World = 10");
+'''),
+
+    ('string', True, {'strict': True},
+            '''
+	print("Hello World = 10");
+'''),
+
+    ('string', True, {'strict': True},
+            '''
+	" (Hello World = 10) "
+'''),
+
+    ('line-breaks', False, {'strict': True},
+            '''
+	return ((i
+=
+10
+test
+));
+'''),
+
+    ('operator ==', True, {'strict': True},
+            '''
+	if (Hello World == 10) "
+'''),
+
+    ('operator !=', True, {'strict': True},
+            '''
+	if (Hello World != 10) "
+'''),
+
+    ('operator >=', True, {'strict': True},
+            '''
+	if (Hello World >= 10) "
+'''),
+
+    ('operator <=', True, {'strict': True},
+            '''
+	if (Hello World <= 10) "
+'''),
+
+    ('operator =!', False, {'strict': True},
+            '''
+	if (Hello World =! 10) "
+'''),
+
+    ('operator =>', False, {'strict': True},
+            '''
+	if (Hello World => 10) "
+'''),
+
+    ('operator =<', False, {'strict': True},
+            '''
+	if (Hello World =< 10) "
+'''),
+
+    ('operator +=', False, {'strict': True},
+            '''
+	if (Hello World += 10) "
+'''),
+
+    ('operator -=', False, {'strict': True},
+            '''
+	if (Hello World -= 10) "
+'''),
+
+    ('operator *=', False, {'strict': True},
+            '''
+	if (Hello World *= 10) "
+'''),
+
+    ('operator /=', False, {'strict': True},
+            '''
+	if (Hello World /= 10) "
+'''),
+
+    ('operator %=', False, {'strict': True},
+            '''
+	if (Hello World %= 10) "
+'''),
+
+
+    ('operator <<=', False, {'strict': True},
+            '''
+	if (Hello World <<= 10) "
+'''),
+
+    ('operator >>=', False, {'strict': True},
+            '''
+	if (Hello World >>= 10) "
+'''),
+
+    ('operator &=', False, {'strict': True},
+            '''
+	if (Hello World &= 10) "
+'''),
+
+    ('operator ^=', False, {'strict': True},
+            '''
+	if (Hello World ^= 10) "
+'''),
+
+    ('operator |=', False, {'strict': True},
+            '''
+	if (Hello World |= 10) "
+'''),
+]
---
+++ tests/test_check_accidental_assignment.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+# SPDX-License-Identifier: MIT
+# SPDX-FileCopyrightText: 2022 Rafael Silva <perigoso@riseup.net>
+from __future__ import annotations
+
+import unittest
+from typing import Sequence
+
+from hooks.check_accidental_assignment import check_accidental_assignment
+from tests.test_cases_check_accidental_assignment import test_cases
+
+
+class TestCheckAccidentalAssignment(unittest.TestCase):
+
+    def test_check_accidental_assignment(self):
+        failed = False
+
+        for test_name, expected, args, input in test_cases:
+            print(f'Running test: {test_name} ... ', end='')
+
+            with open('tempfile', 'w') as f:
+                f.write(input)
+
+            result = check_accidental_assignment('tempfile', quiet=True, **args) == 0
+
+            if result != expected:
+                failed = True
+                print('FAIL')
+            else:
+                print('PASS')
+
+        self.assertEqual(failed, False)
+
+
+if __name__ == '__main__':
+    unittest.main()
